af900a27c7746ca9af667b679b129723541568e4
Chandira77 <irachand620@gmail.com>
2024-03-04 15:05:30 +0545

    inheritance types

---
+++ L_Inheritance/f_single_name_method/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace f_single_name_method;
+class SuperClass
+{
+    public void hi()
+    {
+        Console.WriteLine("Hi, this is super class");
+    }
+}
+class SubClass : SuperClass
+{
+    public new void hi()
+    {
+        Console.WriteLine("Hello, this is sub class");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubClass subo = new SubClass();
+        SuperClass supo = new SubClass();
+        subo.hi();
+        supo.hi();    }
+}
