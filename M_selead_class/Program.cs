cd83d8c6fb71e512aa164c89c2db1f2b67b9c929
Chandira77 <irachand620@gmail.com>
2024-03-10 10:29:54 +0545

    new commit

---
+++ M_selead_class/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace M_selead_class;
+
+//Sealed class
+sealed class SuperClass
+{
+    public void hi()
+    {
+        Console.WriteLine("hi friends, i'm from superclass");
+    }
+}
+class SubClass : SuperClass
+{
+    public void hello()
+    {
+        Console.WriteLine("hi friends, i'm from subclass");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubClass obj = new SubClass();
+        obj.hello();
+        obj.hi();
+    }
+}
