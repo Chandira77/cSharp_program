858e74dca2d1a0e53c10980baf9a63ac65a7ac28
Chandira77 <irachand620@gmail.com>
2024-04-17 16:15:44 +0545

    about virtual method

---
+++ p_virtual_method/a_virtual_method/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace a_virtual_method;
+
+class Superclass
+{
+    public virtual int add(int a, int b)
+    {
+        return(a+b);
+    }
+}
+class Subclass : Superclass
+{
+    public override int add(int a, int b)
+    {
+        return(a+b+5);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Subclass obj = new Subclass();
+        Console.WriteLine("The result =" +obj.add(32,44));
+    }
+}
